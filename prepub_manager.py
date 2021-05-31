9d5224eb9176cfca92683e8288d256a5da950436
firesuiry-bangongshi <4690734@qq.com>
2021-05-31 20:50:41 +0800

    增加百度网盘文件获取方式
    Signed-off-by: firesuiry-bangongshi <4690734@qq.com>

--- prepub_manager.py
+++ prepub_manager.py
@@ -1,13 +1,78 @@
 import os
 import pathlib
+import time
 
 import xlwt
 from PySide2.QtCore import QThread, Signal
 from PySide2.QtWidgets import QFileDialog
 
 from common import cal_md5
+import sqlite3
 
 FILE_XLS_FILE = './要发布文件.xls'
+
+
+class GenerateBaiduPanFileXlsWorker(QThread):
+    log_signal = Signal(str)
+
+    def __init__(self, dbpath):
+        super().__init__()
+        self.dbpath = dbpath
+        self.values = []
+
+    def run(self):
+        self.export_baidupan_file_info()
+        try:
+            self.generate_data_xls()
+        except PermissionError:
+            self.log_signal.emit(f'出现错误 请手动删除 要发布文件.xls 或者关闭wps或excel')
+        except Exception as e:
+            self.log_signal.emit(f'出现错误 {e}')
+
+    def export_baidupan_file_info(self):
+        conn = sqlite3.connect(self.dbpath)
+        cursor = conn.cursor()
+        # 执行查询语句：
+        cursor.execute('select server_filename,file_size,md5,parent_path from cache_file ORDER BY file_size DESC')
+
+        # 使用featchall获得结果集（list）
+        values = cursor.fetchall()
+        # for value in values:
+        #     print(value)
+        self.values = values
+        return values
+
+    def generate_data_xls(self):
+        book = xlwt.Workbook(encoding="utf-8", style_compression=0)
+        sheet = book.add_sheet('test01', cell_overwrite_ok=True)
+        sheet.write(0, 0, '标题')
+        sheet.write(0, 1, '描述')
+        sheet.write(0, 2, '网盘链接')
+        sheet.write(0, 3, '网盘提取码')
+        sheet.write(0, 4, '解压密码')
+        sheet.write(0, 5, '价格')
+        sheet.write(0, 6, '文件类型')
+        sheet.write(0, 7, '文件大小')
+        row = 1
+        for data in self.values:
+            size = data[1]
+            if size < 1024:
+                continue
+            sheet.write(row, 0, data[0])
+            sheet.write(row, 1, f"文件标题：{data[3]}{data[0]}")
+            sheet.write(row, 2, 'PREPUB')
+            sheet.write(row, 5, '1')
+            sheet.write(row, 6, data[0].split('.')[-1] if '.' in data[0] else '')
+            sheet.write(row, 7, '{:.2f}MB'.format(data[1] / 2 ** 20))
+            # self.log_signal.emit(f"正在写入第{row}条数据：{data[0]}")
+            row += 1
+            if row > 65530:
+                self.log_signal.emit(f"文件过多 只能导出前65530个文件")
+                break
+
+        book.save(FILE_XLS_FILE)
+        self.log_signal.emit(f"已经写入完成 共计{row}条数据")
+        self.log_signal.emit(f"请查看上方的介绍完成下一步发布")
 
 
 class GenerateFileXlsWorker(QThread):
@@ -36,9 +101,8 @@ class GenerateFileXlsWorker(QThread):
             sheet.write(row, 5, '1')
             sheet.write(row, 6, data.get('type', ''))
             sheet.write(row, 7, data.get('size', ''))
-            self.log_signal.emit(f"正在写入第{row}条数据：{data.get('name', '')}")
+            # self.log_signal.emit(f"正在写入第{row}条数据：{data.get('name', '')}")
             row += 1
-
         book.save(FILE_XLS_FILE)
         self.log_signal.emit(f"已经写入完成 共计{row}条数据")
         self.log_signal.emit(f"请查看上方的介绍完成下一步发布")
@@ -84,9 +148,10 @@ class PrepubWidget:
         self.main = main
 
         self.window.select_prepub_file_btn.clicked.connect(self.process_directory)
+        self.window.select_baidupan_out_file.clicked.connect(self.process_baidupan_db_file)
 
     def process_directory(self):
-        self.add_log('开始执行本地文件预分享，文件夹选择')
+        self.add_log('开始执行本地文件预分享，文件夹选择，请稍等')
         dir_name = QFileDialog.getExistingDirectory(self.window, "选择要分享的文件夹", r"C:\Users\Administrator\Desktop")
         self.add_log('当前选择的文件夹：')
         self.add_log(dir_name)
@@ -97,6 +162,17 @@ class PrepubWidget:
         self.local_prepub.log_signal.connect(self.add_log)
         self.local_prepub.run()
 
+    def process_baidupan_db_file(self):
+        self.add_log('开始执行百度网盘文件预分享，请稍等')
+        file_names = QFileDialog.getOpenFileName(self.window, "选择分享的文件", r"C:\Users\Administrator\Desktop",
+                                                 '网盘文件目录(BaiduYunCacheFileV0.db)')
+        file_name = file_names[0]
+        self.add_log('当前选择的文件：')
+        self.add_log(file_name)
+
+        self.baidupan_prepub = GenerateBaiduPanFileXlsWorker(file_name)
+        self.baidupan_prepub.log_signal.connect(self.add_log)
+        self.baidupan_prepub.run()
 
     def add_log(self, msg):
         print('添加log：{}'.format(msg))
@@ -105,6 +181,6 @@ class PrepubWidget:
 
 
 if __name__ == '__main__':
-    datas = export_file_info(r'H:\cassann_data0409')
-    gfxls = GenerateFileXlsWorker(datas)
-    gfxls.run()
+    dbpath = r'D:\Users\cass\AppData\Roaming\baidu\BaiduNetdisk\users\eabe70b53624ca13c9dcabe2da9c573a\BaiduYunCacheFileV0.db'
+    p = GenerateBaiduPanFileXlsWorker(dbpath)
+    p.run()
